00eede446284230dac921ddbcfdc1d7f47155e2b
Yernaz Kengesbekov <yernazkenesbekov24@gmail.com>
2026-04-18 02:49:02 +0500

    feat(details): обновлен дизайн программы мероприятий с добавлением фотографий
    - Внедрена двухколоночная (зигзагообразная) структура таймлайна с использованием CSS Grid.
    - Для каждого события (17:00, 18:00, 23:00) добавлены фотографии в круглых рамках.
    - Возвращен оригинальный бордовый фон (бургюнди) с золотым паттерном.
    - Обновлены шрифты таймлайна (Cormorant Garamond) и тексты перекрашены в золотой цвет для лучшего контраста.

--- vite-project/src/components/Details.tsx
+++ vite-project/src/components/Details.tsx
@@ -1,24 +1,53 @@
 import './Details.css';
+
 export default function Details() {
     return (
         <section id="details">
             <div className="details-inner">
-                <h2 className="program-title reveal">Тойдың бағдарламасы</h2>
-                <div className="program-timeline">
-                    <div className="program-row reveal">
-                        <time className="program-time">17:00</time>
-                        <p className="program-desc">Қонақтардың жиналуы</p>
+                <h2 className="program-title reveal">Тойдың бағдарламасы:</h2>
+                
+                <div className="program-grid">
+                    <div className="program-line"></div>
+                    
+                    {/* Item 1 */}
+                    <div className="program-grid-item left-content reveal-left">
+                        <div className="program-img-wrapper">
+                            <img src="/images/photo_2026-04-18_02-27-27_.jpg" alt="17:00" />
+                        </div>
                     </div>
-                    <div className="program-divider" aria-hidden="true"></div>
-                    <div className="program-row reveal">
-                        <time className="program-time">18:00</time>
-                        <p className="program-desc">Тойдың басталуы</p>
+                    <div className="program-grid-item right-content reveal-right">
+                        <div className="program-info">
+                            <time>17:00</time>
+                            <p>ҚОНАҚТАРДЫҢ<br/>ЖИНАЛУЫ</p>
+                        </div>
                     </div>
-                    <div className="program-divider" aria-hidden="true"></div>
-                    <div className="program-row reveal">
-                        <time className="program-time">23:00</time>
-                        <p className="program-desc">Шығарып салу</p>
+
+                    {/* Item 2 */}
+                    <div className="program-grid-item left-content text-right reveal-left">
+                        <div className="program-info">
+                            <time>18:00</time>
+                            <p>ТОЙДЫҢ<br/>БАСТАЛУЫ</p>
+                        </div>
                     </div>
+                    <div className="program-grid-item right-content reveal-right">
+                        <div className="program-img-wrapper">
+                            <img src="/images/photo_2026-04-18_02-27-37_.jpg" alt="18:00" />
+                        </div>
+                    </div>
+
+                    {/* Item 3 */}
+                    <div className="program-grid-item left-content reveal-left">
+                        <div className="program-img-wrapper">
+                            <img src="/images/photo_2026-04-18_02-27-45_.jpg" alt="23:00" />
+                        </div>
+                    </div>
+                    <div className="program-grid-item right-content reveal-right">
+                        <div className="program-info">
+                            <time>23:00</time>
+                            <p>ШЫҒАРЫП<br/>САЛУ</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </section>
